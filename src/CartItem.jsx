53d2f03bf3d0e2472f063778a71d9db870cfc5e8
08-Aryan <123479305+08-Aryan@users.noreply.github.com>
2026-05-24 19:36:47 +0530

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -75,7 +75,12 @@ const handleDecrement = (item) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button 
+    className="get-started-button1"
+    onClick={handleCheckoutShopping}
+>
+    Checkout
+</button>
       </div>
     </div>
   );
