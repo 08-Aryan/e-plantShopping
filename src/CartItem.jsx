05e56f9b9dc54facb8b40116e077092fd8f69144
08-Aryan <1706.aryanjain@gmail.com>
2026-05-24 09:34:16 -0400

    cart-item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,34 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        var total = 0;
+        cart.forEach((item)=>{
+            const quantity = item.quantity;
+            const cost = parseFloat(item.cost.substring(1));
+            total += (cost * quantity);
+        })
+        return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping();
   };
-
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
